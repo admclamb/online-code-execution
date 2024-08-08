85b91958e9ce5d115b5790e195bf182e9e04de47
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-08-07 21:35:57 -0400

    fix axios

--- client/src/CodeCompiler/CodeCompiler.jsx
+++ client/src/CodeCompiler/CodeCompiler.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import Axios from "axios";
+import axios from "axios";
 import CodeMirror from "@uiw/react-codemirror";
 import { monokai } from "@uiw/codemirror-theme-monokai";
 import { javaLanguage } from "@codemirror/lang-java";
